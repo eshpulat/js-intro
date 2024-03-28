90d7459e874c44bcd1bcd7b5e83815c0acd49566
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-03-28 00:26:13 -0400

    Calculate mathematical formulas

---
+++ assignments/assignment-3/Mathematical-formulas.js
@@ -0,0 +1,29 @@
+// 1-...
+// var r = 20 ** 2;
+console.log(r); // 400
+
+var p = 3.14;
+var area = p * r;
+console.log(area); // 1256
+
+// 2-...
+var length = 15;
+var width = 25;
+var sum = length + width; // 40
+
+var perimeter = 2 * sum;
+console.log(perimeter); // 80
+
+// 3-..
+
+var degrees = 30;
+var p = 3.14;
+
+var radians = degrees * (p / 180);
+console.log(radians); // 0.5233333333333334
+
+// 4-..
+var r = 20;
+
+var Perimeter = 2 * p * r;
+console.log(perimeter); // 80

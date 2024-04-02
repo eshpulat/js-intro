a7d97565d99bda48fb93a09176e9e1aaebb97130
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-01 22:14:02 -0400

    Write assignment operations

---
+++ assignments/assignment-4/operations.js
@@ -0,0 +1,16 @@
+var num1 = 10;
+var num2 = 20;
+var total = 0;
+
+total += num1;
+total += num2;
+console.log(total);
+
+num1 -= total;
+console.log(total);
+
+total *= num2;
+console.log(total);
+
+total /= num1;
+console.log(total);

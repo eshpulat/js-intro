cc9cb4703df252ae370788b77f3818457e8979bb
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-01 23:43:17 -0400

    Finish template literals

---
+++ assignments/assignment-6/myself-homework.js
@@ -0,0 +1,16 @@
+var hello = "Hello";
+var name = "my name is Muhammad";
+var age = "and I am 25 years old.";
+var city = "I live in New York,";
+var job = "where I work as a Uber driver.";
+var hobby =
+    "In my free time, I enjoy whatching the movie and playing the table tennis.";
+
+var food = "My favorite food is pizza";
+var color = "and my favorite color is red.";
+var travel = "My dream is to one day visit Tokyo.";
+var season =
+    "My favorite season of the year is autumn because of the beautiful changing colors of the leaves.";
+
+var aboutMe = `${hello} ${name} ${age} ${city} ${job} ${hobby} ${food} ${color} ${travel} ${season}`;
+console.log(aboutMe);

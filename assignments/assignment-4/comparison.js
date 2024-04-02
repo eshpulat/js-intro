8c4766ce88864b551f5642af1092d7ce9f173186
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-01 22:27:35 -0400

    Calculate boolean comparison

---
+++ assignments/assignment-4/comparison.js
@@ -0,0 +1,13 @@
+var num1 = 50;
+var num2 = 60;
+var total = 10;
+
+// Compare if num1 is greater than num2 and print the boolean result.
+console.log(num1 > num2); // false
+
+// Determine if num1 is less than or equal to num2 and print the boolean result.
+console.log(num1 <= num2); // true
+
+// Check whether num1 multiplied by num2 is greater than or equal to total and print the boolean outcome.
+var sum = num1 * num2 >= total;
+console.log(sum);

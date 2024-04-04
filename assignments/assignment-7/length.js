260264d555b6a001ff88fd45ddb2edd9e35c1bd1
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-04 01:26:37 -0400

    Compare lengths method

---
+++ assignments/assignment-7/length.js
@@ -0,0 +1,14 @@
+var post1 =
+    "This assignment is designed to deepen your understanding of string manipulation in JavaScript, focusing on various methods and properties available for strings";
+console.log(post1.length);
+
+var post2 =
+    "Imagine you are creating a feature for a social media application that compares the length of two posts to determine which one is longer, or if they are of equal length";
+console.log(post2.length);
+
+var comparisonResult1 = post1.length;
+var comparisonResult2 = post2.length;
+
+console.log(comparisonResult1 === comparisonResult2);
+console.log(comparisonResult1 < comparisonResult2);
+console.log(comparisonResult1 > comparisonResult2);

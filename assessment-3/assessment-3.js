5b1683e4f417838940e490457a12ad887bcc4bb2
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-07 15:25:39 -0400

    finish assessment

--- assessment-3/assessment-3.js
+++ assessment-3/assessment-3.js
@@ -2,35 +2,36 @@
 // Remove spaces
 
 var space = "I Love JavaScript";
-var result = space.trim();
+var result = space.replaceAll(" ", "");
 
-console.log(result.includes(" "));
+console.log(result);
 
 // Challenge-2
 
 var javascript = "javascript";
-var anotherjavascript = javascript.replace("javascript", "JavaScript");
+var anotherjavascript = javascript.replace("j", "J").replace("s", "S");
 console.log(anotherjavascript);
 
 // Challenge-3
 
 var fullName = "Muhammad Eshpulatov";
-var initial = fullName.toUpperCase().slice(0, 1);
-console.log(initial);
+var initial = fullName[0];
+var initial1 = fullName[9];
+var result = initial.concat(initial1);
+console.log(result);
 
 // Challenge-4
 
 var alphabet = "abcdefghijklomnpqrstuvwxyz";
-var alphabetWithoutMyNameFirstCharacter = alphabet.replace(
-    "abcdefghijklomnpqrstuvwxyz",
-    "abcdefghijklonpqrstuvwxyz"
-);
+var alphabetWithoutMyNameFirstCharacter = alphabet.replace("m", "");
 
 console.log(alphabetWithoutMyNameFirstCharacter);
 
 //  Challenge-5
 
 var completeSentence = "The quick brown fox jumps over the lazy dog.";
+var randomChar = "o";
+var missingRandomCharString;
 
 // Challenge-6
 var randomString = "Exptected";

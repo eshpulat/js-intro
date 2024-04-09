09df1f401ce058328272b92efe5248045b2e7734
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-09 01:21:12 -0400

    Finish random numbers

---
+++ assignments/assignment-10/random.js
@@ -0,0 +1,52 @@
+// Between 1 and 10
+
+var random1 = Math.floor(Math.random() * 10) + 1;
+console.log(random1);
+
+// Between 1 and 100
+
+var random2 = Math.floor(Math.random() * 100) + 1;
+console.log(random2);
+
+// Between 50 and 100
+
+var random3 = Math.floor(Math.random() * 50) + 50;
+console.log(random3);
+
+// Between -10 and 10
+
+var random4 = Math.floor(Math.random() * 20) - 10;
+console.log(random4);
+
+// Between -100 and 100
+
+var random5 = Math.floor(Math.random() * 200) - 100;
+console.log(random5);
+
+// Between 1000 and 2000
+
+var random6 = Math.floor(Math.random() * 1000) + 1000;
+console.log(random6);
+
+// Between -1000 and 0
+
+var random7 = Math.floor(Math.random() * 1000) - 1000;
+console.log(random7);
+
+// Between 1 and 1000
+
+var random8 = Math.floor(Math.random() * 1000) + 1;
+console.log(random8);
+
+// Between -500 and 500
+
+var random9 = Math.floor(Math.random() * 1000) - 500;
+console.log(random9);
+
+// Between 10000 and 20000
+
+var random10 = Math.floor(Math.random() * 10000) + 10000;
+console.log(random10);
+
+// i use the formula for (plus) numbers
+//   var random = Math.floor(Math.random() * (max-min)) + min;

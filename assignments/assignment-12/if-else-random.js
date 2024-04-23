2fd299b576ee8326b2f0c5f793472b0cc756b11e
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-22 22:12:50 -0400

    Update assignment-12

--- assignments/assignment-12/if-else-random.js
+++ assignments/assignment-12/if-else-random.js
@@ -1,10 +1,10 @@
 // Generate a Random User Name
 
-var random = Math.floor(Math.random() * 10) + 1;
+var randomNumber1 = Math.floor(Math.random() * 10) + 1;
 
-if (random >= 1 && random <= 3) {
+if (randomNumber1 >= 1 && randomNumber1 <= 3) {
     console.log("Bob");
-} else if (random >= 4 && random <= 7) {
+} else if (randomNumber1 >= 4 && randomNumber1 <= 7) {
     console.log("Martin");
 } else {
     console.log("Jennifer");
@@ -12,18 +12,18 @@ if (random >= 1 && random <= 3) {
 
 // Select a Greeting Message Template
 
-var randomnumber = Math.ceil(Math.random() * 4);
+var randomNumber2 = Math.ceil(Math.random() * 4);
 var name = "Muhammad";
 
-if (randomnumber === 1) {
+if (randomNumber2 === 1) {
     console.log(
         `Dear ${name}, wishing you a day filled with happiness and a year filled with joy.`
     );
-} else if (randomnumber === 2) {
+} else if (randomNumber2 === 2) {
     console.log(
         `Dear ${name}, sending you smiles for every moment of your special day.`
     );
-} else if (randomnumber === 3) {
+} else if (randomNumber2 === 3) {
     console.log(`Dear ${name}, have a wonderful time and a very happy day!`);
 } else {
     console.log(

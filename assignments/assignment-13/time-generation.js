63827c86ae9913ce4f686db71f3b5b5bfcfb038c
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-22 22:05:35 -0400

    Update assignment-13

--- assignments/assignment-13/time-generation.js
+++ assignments/assignment-13/time-generation.js
@@ -1,14 +1,38 @@
 // Random Time Generation
 
-// Greeting Message Based on Time
-var random = Math.floor(Math.random() * 25);
+var totalSeconds = Math.floor(Math.random() * 86400);
+console.log(`Total seconds: ${totalSeconds}`);
 
-if (random >= 0 && random <= 12) {
+// Calculating Hours, Minutes, and Seconds
+var hours = Math.floor(totalSeconds / 3600);
+var remainingSeconds = totalSeconds % 3600;
+var minutes = Math.floor(remainingSeconds / 60);
+var seconds = remainingSeconds % 60;
+
+// Formatting Hours, Minutes, and Seconds
+var formattedHours = `${hours}`;
+var formattedMinutes = `${minutes}`;
+var formattedSeconds = `${seconds}`;
+
+if (hours < 10) {
+    formattedHours = `0${hours}`;
+}
+if (minutes < 10) {
+    formattedMinutes = `0${minutes}`;
+}
+if (seconds < 10) {
+    formattedSeconds = `0${seconds}`;
+}
+
+// Printing the Timestamp
+var timestamp = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+console.log(timestamp);
+
+// Printing a Greeting Based on the Hour
+if (hours < 12) {
     console.log("Good morning!");
-} else if (random >= 12 && random <= 17) {
+} else if (hours < 17) {
     console.log("Good afternoon!");
 } else {
     console.log("Good evening!");
 }
-
-// Examples of Outpu

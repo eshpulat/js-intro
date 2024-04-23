79f02f63702604a843be5cd2f0a99ef04dfbe27b
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-04-22 22:20:53 -0400

    Update assignment-11

--- assignments/assignment-11/if-else.js
+++ assignments/assignment-11/if-else.js
@@ -1,9 +1,9 @@
 // Password Correctness Check
 
-var password1 = "Enes-aridenez";
-var password2 = " Ahmat-enes";
+var correctPassword = "Enes-aridenez";
+var userInputPassword = " Ahmat-enes";
 
-if (password1 === password2) {
+if (correctPassword === userInputPassword) {
     console.log("Access Granted.");
 } else {
     console.log("Access Denied. Incorrect Password.");
@@ -11,10 +11,9 @@ if (password1 === password2) {
 
 // Age Verification for Content Access
 
-var age1 = 18;
-var age2 = 18;
+var userAge = 20;
 
-if (age1 <= age2) {
+if (userAge >= 18) {
     console.log("You have access to the content.");
 } else {
     console.log("Access restricted. You must be 18 years or older.");
@@ -23,7 +22,7 @@ if (age1 <= age2) {
 // Checking Input Length
 var username = "Muhammad";
 
-if (username.length === 5) {
+if (username.length >= 5) {
     console.log("Username is valid.");
 } else {
     console.log("Username must be at least 5 characters long.");
@@ -32,10 +31,9 @@ if (username.length === 5) {
 // Simple Quiz
 // question 2 + 2
 
-var randomanswer = 1;
-var correctanswer = 2;
-
-if (correctanswer === randomanswer) {
+var correctAnswer = "42"; // Store the correct answer to a question
+var userAnswer = "42"; // Simulate a user's answer
+if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
     console.log("Correct! You may proceed.");
 } else {
     console.log("Incorrect answer. Please try again.");

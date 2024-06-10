53eaeaf002452796774ac66824eb39ceebd1eaf9
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-06-10 11:55:35 -0400

    Update split method

--- assignments/assignment-25/assignment-25.js
+++ assignments/assignment-25/assignment-25.js
@@ -14,3 +14,13 @@ console.log(limited);
 
 var limitedWithChar = str.split("", 3);
 console.log(limitedWithChar);
+
+var phrase = "National Aeronautics and Space Administration";
+var words = phrase.split(" ");
+var abbreviation = "";
+
+for (var i = 0; i < words.length; i++) {
+    abbreviation += words[i].charAt(0);
+}
+
+console.log(abbreviation); // Output: "NASA"
